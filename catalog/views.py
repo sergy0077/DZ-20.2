9804268277f3b12bb392405cd237fb0362872f43
Sergey Kutsenko <kutsenkosergey@mail.ru>
2023-08-09 18:46:35 +0300

    не могу открыть наследуемую страницу товара

--- catalog/views.py
+++ catalog/views.py
@@ -11,17 +11,9 @@ def contacts(request):
     return render(request, 'catalog/contacts.html')
 
 
-# def product_detail(request, product_id):
-#     product = get_object_or_404(Product, pk=product_id)
-#     return render(request, 'catalog/product_detail.html', {'product': product})
-
-
 def product_detail(request, product_id):
-    print(f"product_id: {product_id}")
     try:
         product = Product.objects.get(pk=product_id)
     except Product.DoesNotExist:
-        print("Product does not exist")
         raise Http404("Product does not exist")
-    print(f"Product name: {product.name}")
     return render(request, 'catalog/product_detail.html', {'product': product})
